c36df91923e95b380602b79fb449fd29c29fbe5f
Vincenzo Ingrao Jr <vincenzoingraojr@gmail.com>
2021-07-04 15:16:05 +0200

    Commit

--- website/client/pages/index.tsx
+++ website/client/pages/index.tsx
@@ -9,8 +9,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>
-        Hello!
+      <div className="container">
+        <div className="container-item">
+          <div className="site-title">The Scientific Place</div>
+          <div>
+            This is a place where scientists and science lovers can share ideas, research papers and projects.
+          </div>
+        </div>
+        <div className="container-item">
+          <div className="authentication-form">
+            Login form
+          </div>
+        </div>
       </div>
 
       <footer>
