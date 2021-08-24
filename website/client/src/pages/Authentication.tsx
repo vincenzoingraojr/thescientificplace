e262a5dafda845b20a0684b74fad02f0988bf16e
Vincenzo Ingrao Jr <vincenzoingraojr@gmail.com>
2021-08-24 08:39:54 +0200

    Footer added

--- website/client/src/pages/Authentication.tsx
+++ website/client/src/pages/Authentication.tsx
@@ -1,15 +1,20 @@
 function Authentication() {
     return (
-        <div className="auth-container">
-            <div className="container-item">
-                <div className="site-title">
-                    The Scientific Place
-                </div>
-                <div>
-                    This is a place where scientists and science lovers can share ideas, research papers and projects.
+        <>
+            <div className="auth-container">
+                <div className="container-item">
+                    <div className="site-title">
+                        The Scientific Place
+                    </div>
+                    <div>
+                        This is a place where scientists and science lovers can share ideas, research papers and projects.
+                    </div>
                 </div>
             </div>
-        </div>
+            <footer>
+                &copy; 2021 The Scientific Place
+            </footer>
+        </>
     );
 }
 
