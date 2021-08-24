73fbb8f7f348b26bc7a319f837e50b6707c8c284
Vincenzo Ingrao Jr <vincenzoingraojr@gmail.com>
2021-08-24 20:50:15 +0200

    Commit

--- website/client/src/pages/Authentication.tsx
+++ website/client/src/pages/Authentication.tsx
@@ -10,6 +10,15 @@ function Authentication() {
                         This is a place where scientists and science lovers can share ideas, research papers and projects.
                     </div>
                 </div>
+                <div className="container-item">
+                    <form className="login-form">
+                        <input type="text" placeholder="Username" />
+                        <input type="password" placeholder="Password" />
+                        <button className="login" type="submit">Log in</button>
+                    </form>
+                    <div className="line"></div>
+                    <button className="signup">Sign up</button>
+                </div>
             </div>
             <footer>
                 &copy; 2021 The Scientific Place
