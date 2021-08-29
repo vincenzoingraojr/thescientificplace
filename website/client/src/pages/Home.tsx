8c8bb67d4a0da59c847735a2e1fcb5e9ae31805b
Vincenzo Ingrao Jr <vincenzoingraojr@gmail.com>
2021-08-29 18:29:12 +0200

    Commit

--- website/client/src/pages/Home.tsx
+++ website/client/src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { Icon } from "@fluentui/react/lib/Icon";
+import { Link } from "react-router-dom";
 import { useMeQuery } from "../generated/graphql";
 
 function Home() {
@@ -5,7 +7,42 @@ function Home() {
 
     return (
         <div className="page-container">
-            Home. Hello, {data?.me?.firstName}
+            <div className="sidebar">
+                <div className="site-title-sidebar">
+                    <Link to="/home">
+                        The Scientific Place
+                    </Link>
+                </div>
+                <nav className="sidebar-nav">
+                    <Link to="/home">
+                        <div className="sidebar-nav-entry">
+                            <div className="nav-entry-icon">
+                                <Icon iconName="Home" />
+                            </div>
+                            <div className="nav-entry-name">
+                                Home
+                            </div>
+                        </div>
+                    </Link>
+                </nav>
+            </div>
+            <header className="page-header">
+                <div className="page-title">
+                    Home
+                </div>
+                <div className="search-bar">
+                    <Icon iconName="Search" />
+                    <input type="text" placeholder="Search, explore" />
+                </div>
+            </header>
+            <div className="main-container">
+                <div className="main-feed">
+                    Home. Hello, {data?.me?.firstName}
+                </div>
+                <div className="right-column">
+                    Right sidebar.
+                </div>
+            </div>
         </div>
     );
 }
