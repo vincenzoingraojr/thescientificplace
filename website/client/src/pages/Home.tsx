ec3a94cdc8094c63e357223f04d221b191668582
Vincenzo Ingrao Jr <vincenzoingraojr@gmail.com>
2021-08-29 16:58:17 +0200

    Commit

--- website/client/src/pages/Home.tsx
+++ website/client/src/pages/Home.tsx
@@ -1,5 +1,3 @@
-import { Icon } from '@fluentui/react/lib/Icon';
-import { Link } from "react-router-dom";
 import { useMeQuery } from "../generated/graphql";
 
 function Home() {
@@ -7,43 +5,7 @@ function Home() {
 
     return (
         <div className="page-container">
-            <div className="left-sidebar">
-                <div className="site-title-sidebar">
-                    <Link to="/home">
-                        The Scientific Place
-                    </Link>
-                </div>
-                <div className="site-title-sidebar-circle">
-                    <Link to="/home">
-                        <Icon iconName="FullCircleMask" />
-                    </Link>
-                </div>
-                <nav className="nav-sidebar">
-                    <Link to="/home">
-                        <div className="nav-entry">
-                            <div className="nav-entry-icon">
-                                <Icon iconName="Home" />
-                            </div>
-                            <div className="nav-entry-name">
-                                Home
-                            </div>
-                        </div>
-                    </Link>
-                </nav>
-            </div>
-            <div className="page-title">Home</div>
-            <div className="fixed-searchbar">
-                <Icon iconName="Search" />
-                <input type="text" placeholder="Search, explore" />
-            </div>
-            <div className="main-container">
-                <div className="main-feed">
-                   Hello, {data?.me?.firstName}
-                </div>
-                <div className="right-sidebar">
-                    Right sidebar.
-                </div>
-            </div>
+            Home. Hello, {data?.me?.firstName}
         </div>
     );
 }
