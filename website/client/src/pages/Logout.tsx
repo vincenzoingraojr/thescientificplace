6d9b5399e11a03c69211f1bb015b55f4cacb65b5
Vincenzo Ingrao Jr <vincenzoingraojr@gmail.com>
2021-08-30 15:09:05 +0200

    Logout completed

--- website/client/src/pages/Logout.tsx
+++ website/client/src/pages/Logout.tsx
@@ -1,3 +1,4 @@
+import { Icon } from "@fluentui/react/lib/Icon";
 import { useHistory } from "react-router-dom";
 import { setAccessToken } from "../accessToken";
 import { useLogoutMutation, useMeQuery } from "../generated/graphql";
@@ -5,26 +6,38 @@ import { useLogoutMutation, useMeQuery } from "../generated/graphql";
 function Logout() {
     const [logout, { client }] = useLogoutMutation();
 
-    const history = useHistory();
-
     const { data } = useMeQuery({ fetchPolicy: "network-only" });
+
+    const history = useHistory();
     
     return (
         <div className="logout-page">
-            {data && data.me ? (
-                <button
-                    onClick={async () => {
-                        await logout();
-                        setAccessToken("");
-                        await client!.resetStore();
+            <div className="logout-content-container">
+                <div className="logout-content-header">
+                    The Scientific Place
+                </div>
+                <div className="logout-content">
+                    <div className="logout-content-text">
+                        Do you want to disconnect from <b>@{data?.me?.username}</b>?
+                    </div>
+                    <button
+                        onClick={async () => {
+                            await logout();
+                            setAccessToken("");
+                            await client!.resetStore();
 
-                        history.go(0);
-                    }}
-                    className="logout"
-                >
-                    Log out
-                </button>
-            ) : null}
+                            history.go(0);
+                        }}
+                        className="logout margin-bottom-24"
+                    >
+                        Log out
+                    </button>
+                    <div className="go-back" onClick={() => history.goBack()}>
+                        <Icon iconName="ChromeBack" />
+                        Go back
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
