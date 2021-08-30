05985b5955e773cf0262be62c211691d6c5be102
Vincenzo Ingrao Jr <vincenzoingraojr@gmail.com>
2021-08-30 15:34:49 +0200

    Update Head.tsx

--- website/client/src/components/Head.tsx
+++ website/client/src/components/Head.tsx
@@ -2,13 +2,15 @@ import { FunctionComponent } from "react";
 import { Helmet } from "react-helmet";
 
 export interface HeadProps {
-    title: String;
+    title: string;
 }
 
 const Head: FunctionComponent<HeadProps> = ({ title }) => {
     return (
         <Helmet>
             <title>{title}</title>
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content="The social network of scientists." />
         </Helmet>
     );
 }
