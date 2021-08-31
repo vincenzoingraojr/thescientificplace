c3fb4dbd98a77b5fd1252dfd9c6b7f242efc7ef5
Vincenzo Ingrao Jr <vincenzoingraojr@gmail.com>
2021-08-31 10:14:04 +0200

    Commit

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NotFound from "./pages/404";
 import Home from "./pages/Home";
-import Response from "./pages/Response";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/response" component={Response} />
         <Route path="*" component={NotFound} />
       </Switch>
     </BrowserRouter>
--- landing/src/pages/Home.tsx
+++ landing/src/pages/Home.tsx
@@ -3,25 +3,11 @@ function Home() {
         <div className="landing-container">
             <div className="landing-inner-container">
                 <div className="site-title">The Scientific Place</div>
-                <div className="landing-content-text margin-bottom-24">
+                <div className="landing-content-text">
                     The Scientific Place is a social network dedicated to scientists and those who love science. If you need to contact me, you can send me an <a href="mailto:vincenzo@thescientificplace.com" title="My personal email.">email</a>. <br />
                     <br />
-                    If you want to see the progress of the work, you can visit the <a href="https://github.com/vincenzoingraojr/thescientificplace" target="_blank" rel="noreferrer" title="The GitHub repository of the social network.">GitHub repository</a> of the social network. <br />
-                    <br />
-                    You can also contact me by filling out the form below.
+                    If you want to see the progress of the work, you can visit the <a href="https://github.com/vincenzoingraojr/thescientificplace" target="_blank" rel="noreferrer" title="The GitHub repository of the social network.">GitHub repository</a> of the social network.
                 </div>
-                <form name="contact" method="POST" data-netlify="true" action="/response">
-                    <input type="hidden" name="form-name" value="contact" />
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" />
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" />
-                    <label htmlFor="subject">Subject</label>
-                    <input type="text" id="subject" name="subject" />
-                    <label htmlFor="message">Message</label>
-                    <textarea name="message" id="message" rows={3} />
-                    <button type="submit">Send</button>
-                </form>
             </div>
         </div>
     );
